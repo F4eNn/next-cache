e8e3f073b55951fc5fd0ed82fd6eec47f87af9d1
Mateusz <mateusz4k@outlook.com>
2023-11-26 11:57:37 +0100

    test4

--- next.config.mjs
+++ next.config.mjs
@@ -1,15 +1,12 @@
-/** @format */
-
-import withPlaiceholder from '@plaiceholder/next';
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-	images: {
-		remotePatterns: [
-			{ hostname: 'localhost', port: '1337', pathname: '/uploads/**', protocol: 'http' },
-			{ hostname: 'res.cloudinary.com', pathname: `/${process.env.NEXT_CLOUDINARY_NAME}/**`, protocol: 'https' },
-		],
-	},
-};
+    images: {
+        remotePatterns: [
+           { hostname: 'localhost', port: '1337', pathname: '/uploads/**', protocol: 'http' },
+           { hostname: 'res.cloudinary.com', pathname: `/${process.env.NEXT_CLOUDINARY_NAME}/**`, protocol: 'https' },
+        ],
+     },
+}
 
-export default withPlaiceholder(nextConfig);
+module.exports = nextConfig
