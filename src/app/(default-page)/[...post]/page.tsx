e85dd84fcdc112edbd1d2939cd9555167281559b
Mateusz <mateusz4k@outlook.com>
2023-11-26 11:38:09 +0100

    test

--- src/app/(default-page)/[...post]/page.tsx
+++ src/app/(default-page)/[...post]/page.tsx
@@ -27,6 +27,35 @@ export interface IPostData {
 	galeria: { data: RootDataType<ImageType>[] };
 }
 
+// import { IPostData } from '@/app/(default-site)/[...post]/page';
+
+// export const getBase64 = async (image: string) => {
+// 	try {
+// 		const res = await fetch(image);
+// 		if (!res.ok) {
+// 			throw new Error(`Failed to fetch image ${res.status} ${res.statusText}`);
+// 		}
+// 		const buffer = await res.arrayBuffer();
+
+// 		const { base64 } = await getPlaiceholder(Buffer.from(buffer));
+// 		return base64;
+// 	} catch (error) {
+// 		console.error(error);
+// 	}
+// };
+
+// export const getBase64ForAllImg = async (images: IPostData['galeria']): Promise<IPostData['galeria']['data']> => {
+// 	const base64Promises = images.data.map(img => getBase64(img.attributes.url));
+
+// 	const base64Result = await Promise.all(base64Promises);
+
+// 	const photosWithBlur = images.data.map((img, idx) => {
+// 		img.attributes.blurDataUrl = base64Result[idx];
+// 		return img;
+// 	});
+// 	return photosWithBlur;
+// };
+
 const getPost = async (param: string): Promise<any> => {
 	try {
 		const res = await fetch(
@@ -62,15 +91,53 @@ const PostPage = async ({ params }: { params: { post: string[] } }) => {
 	}
 	const { publishedAt, tytul, zawartosc_posta, zdjecie_glowne, galeria } = data.attributes;
 
-	return <main className='mb-32'>{tytul}
-    <Image src={galeria.data[0].attributes.url} width={400} height={500} className='h-56 w-56 object-cover' alt=''/>
-    <Image src={galeria.data[1].attributes.url} width={400} height={500} className='h-56 w-56 object-cover' alt=''/>
-    <Image src={galeria.data[2].attributes.url} width={400} height={500} className='h-56 w-56 object-cover' alt=''/>
-    <Image src={galeria.data[3].attributes.url} width={400} height={500} className='h-56 w-56 object-cover' alt=''/>
-    <Image src={galeria.data[4].attributes.url} width={400} height={500} className='h-56 w-56 object-cover' alt=''/>
-    <Image src={galeria.data[5].attributes.url} width={400} height={500} className='h-56 w-56 object-cover' alt=''/>
-    </main>;
-   
+	return (
+		<main className='mb-32'>
+			{tytul}
+			<Image
+				src={galeria.data[0].attributes.url}
+				width={400}
+				height={500}
+				className='h-56 w-56 object-cover'
+				alt=''
+			/>
+			<Image
+				src={galeria.data[1].attributes.url}
+				width={400}
+				height={500}
+				className='h-56 w-56 object-cover'
+				alt=''
+			/>
+			<Image
+				src={galeria.data[2].attributes.url}
+				width={400}
+				height={500}
+				className='h-56 w-56 object-cover'
+				alt=''
+			/>
+			<Image
+				src={galeria.data[3].attributes.url}
+				width={400}
+				height={500}
+				className='h-56 w-56 object-cover'
+				alt=''
+			/>
+			<Image
+				src={galeria.data[4].attributes.url}
+				width={400}
+				height={500}
+				className='h-56 w-56 object-cover'
+				alt=''
+			/>
+			<Image
+				src={galeria.data[5].attributes.url}
+				width={400}
+				height={500}
+				className='h-56 w-56 object-cover'
+				alt=''
+			/>
+		</main>
+	);
 };
 
 export default PostPage;
